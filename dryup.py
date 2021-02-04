ad4e32cb8b088d116a0862158df403602d22f8a0
finlayhenderson <henderson.finlay.0@gmail.com>
2021-02-04 09:44:19 +0000

    v1.0.0.

--- dryup.py
+++ dryup.py
@@ -1,29 +1,37 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 
 
 class Menubar:
 
     def __init__(self, parent):
-        font_specs = ("ubuntu", 12)
+        font_specs = ("ubuntu", 11)
 
         menubar = tk.Menu(parent.master, font=font_specs)
         parent.master.config(menu=menubar)
 
-        file_dropdown = tk.Menu(menubar, font=font_specs, tearoff=0)
-        file_dropdown.add_command(label="New File",
+        file = tk.Menu(menubar, font=font_specs, tearoff=0)
+        file.add_command(label="New File",
                                      command=parent.new_file)
-        file_dropdown.add_command(label="Open File",
+        file.add_command(label="Open File",
                                      command=parent.open_file)
-        file_dropdown.add_command(label="Save File",
+        file.add_command(label="Save File",
                                      command=parent.save_file)
-        file_dropdown.add_command(label="Save As",
+        file.add_command(label="Save As",
                                      command=parent.save_as)
-        file_dropdown.add_separator()
-        file_dropdown.add_command(label="Exit",
+        file.add_separator()
+        file.add_command(label="Exit",
                                      command=parent.master.destroy)
 
-        menubar.add_cascade(label="File", menu=file_dropdown)
+        about = tk.Menu(menubar, font=font_specs, tearoff=0)
+        about.add_command(label='Release Notes', command=parent.release_notes)
+        about.add_separator()
+        about.add_command(label='About', command=parent.about)
+        about.add_command(label='Credits', command=parent.credits)
+
+        menubar.add_cascade(label="File", menu=file)
+        menubar.add_cascade(label="Help", menu=about)
 
 
 class DryUp:
@@ -73,10 +81,42 @@ class DryUp:
             self.set_window_title(self.filename)   
 
     def save_file(self):
-        pass
+        if self.filename:
+            try:
+                pass
+            except Exception as e:
+                print(e)
+        else:
+            self.save_as
 
     def save_as(self):
-        pass
+        try:
+            new_file = filedialog.asksaveasfilename(
+                initialfile="Untitled.txt",
+                defaultextension=".txt",
+                filetypes=[("All Files", "*.*"), 
+                        ("Text Files", "*.txt"),
+                        ("Python Scripts", "*.py"),
+                        ("Markdown Documents", "*.md"),
+                        ("JavaScript Files", "*.js"),
+                        ("HTML Files", "*.html"),
+                        ("CSS Files", "*.css")])
+            textarea_content = self.textarea.get(1.0, tk.END)
+            with open(new_file, "w") as f:
+                f.write(textarea_content)
+            self.filename = new_file
+            self.set_window_title(self.filename)
+        except Exception as e:
+            messagebox.showerror('Error!', e)
+
+    def release_notes(self):
+        messagebox.showinfo('Release Notes!', 'None Avalible')
+
+    def credits(self):
+        messagebox.showinfo('Credits', 'Made and developed by Spiiral-Dev (GitHub)')
+
+    def about(self):
+        messagebox.showinfo('About', 'DryUp is a super lightweight and free text editor, with 0 telemetry. Nada, nothing.')
 
 
 
