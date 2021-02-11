c86caecfde8fda86c2dbdfec9094b53ec3459cef
finlayhenderson <henderson.finlay.0@gmail.com>
2021-02-11 09:12:07 +0000

    possible fix

--- dryup.py
+++ dryup.py
@@ -6,23 +6,27 @@ from tkinter import messagebox
 class Menubar:
 
     def __init__(self, parent):
-        font_specs = ("ubuntu", 11)
+        font_specs = ("Helvetica", 9)
 
         menubar = tk.Menu(parent.master, font=font_specs)
         parent.master.config(menu=menubar)
 
         file = tk.Menu(menubar, font=font_specs, tearoff=0)
         file.add_command(label="New File",
-                                     command=parent.new_file)
+                         accelerator="Ctrl+N",
+                         command=parent.new_file)
         file.add_command(label="Open File",
-                                     command=parent.open_file)
+                         accelerator="Ctrl+O",
+                         command=parent.open_file)
         file.add_command(label="Save File",
-                                     command=parent.save_file)
+                         accelerator="Ctrl+S",
+                         command=parent.save_file)
         file.add_command(label="Save As",
-                                     command=parent.save_as)
+                         accelerator="Ctrl+Shift+S",
+                         command=parent.save_as)
         file.add_separator()
         file.add_command(label="Exit",
-                                     command=parent.master.destroy)
+                         command=parent.master.destroy)
 
         about = tk.Menu(menubar, font=font_specs, tearoff=0)
         about.add_command(label='Release Notes', command=parent.release_notes)
@@ -37,9 +41,13 @@ class Menubar:
 class Statusbar:
 
     def __init__(self, parent):
+        font_specs = ("Helvetica", 9)
 
         self.status = tk.StringVar()
         self.status.set("DryUp - 1.0.2 Ruload")
+
+        label = tk.Label(parent.textarea, textvariable=self.status, fg="black", bg="lightgrey", anchor='sw', font=font_specs)
+        label.pack(side=tk.BOTTOM, fill=tk.BOTH)
 
 
 class DryUp:
@@ -48,7 +56,7 @@ class DryUp:
         master.title("Untitled - DryUp")
         master.geometry("1200x700")
 
-        font_specs = ("ubuntu", 18)
+        font_specs = ("Helvetica", 18)
 
         self.master = master
         self.filename = None
@@ -62,18 +70,20 @@ class DryUp:
         self.menubar = Menubar(self)
         self.statusbar = Statusbar(self)
 
+        self.bind_shortcuts()
+
     def set_window_title(self, name=None):
         if name:
             self.master.title(name + " - DryUp")
         else:
             self.master.title("Untitled - DryUp") 
 
-    def new_file(self):
+    def new_file(self, *args):
         self.textarea.delete(1.0, tk.END)
         self.filename = None
         self.set_window_title()
 
-    def open_file(self):
+    def open_file(self, *args):
         self.filename = filedialog.askopenfilename(
             defaultextension=".txt",
             filetypes=[("All Files", "*.*"), 
@@ -89,18 +99,19 @@ class DryUp:
                 self.textarea.insert(1.0, f.read())
             self.set_window_title(self.filename)   
 
-    def save_file(self):
+    def save_file(self, *args):
         if self.filename:
             try:
                 textarea_content = self.textarea.get(1.0, tk.END)
                 with open(self.filename, "w") as f:
                     f.write(textarea_content)
+                # self.statusbar.update_status(True)
             except Exception as e:
                 messagebox.showerror(e)
         else:
             self.save_as()
 
-    def save_as(self):
+    def save_as(self, *args):
         try:
             new_file = filedialog.asksaveasfilename(
                 initialfile="Untitled.txt",
@@ -117,6 +128,7 @@ class DryUp:
                 f.write(textarea_content)
             self.filename = new_file
             self.set_window_title(self.filename)
+            # self.statusbar.update_status(True)
         except Exception as e:
             messagebox.showerror('Error!', e)
 
@@ -129,9 +141,11 @@ class DryUp:
     def about(self):
         messagebox.showinfo('About', 'DryUp is a super lightweight and free text editor, with 0 telemetry. Nada, nothing.')
 
-
-
-
+    def bind_shortcuts(self):
+        self.textarea.bind('<Control-n>', self.new_file)
+        self.textarea.bind('<Control-o>', self.open_file)
+        self.textarea.bind('<Control-s>', self.save_file)
+        self.textarea.bind('<Control-S>', self.save_as)
 
 if __name__ == "__main__":
     master = tk.Tk()
